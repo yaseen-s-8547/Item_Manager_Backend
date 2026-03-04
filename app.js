f06932293984bdfb26c14ea9e6cd8dceba940f24
yaseen-8547 <ycs613678@gmail.com>
2026-03-04 21:19:46 +0530

    mongodb connected

--- app.js
+++ app.js
@@ -1,43 +1,45 @@
+
+import mongoose from "mongoose"
 require ("dotenv").config()
 const express=require("express")
 const cors=(require("cors"))
 const app=express()
 app.use(cors())
 app.use(express.json())
-
-let items=[]
-app.get('/items',(req,res)=>{
+mongoose.connect("mongodb://localhost:27017/mydb")
+const itemSchema=new mongoose.Schema({name:String})
+const Item=mongoose.model("item",itemSchema)
+app.get('/items', async (req,res)=>{
+    const items=await Item.find()
     res.json(items)
 })
-app.post('/items',(req,res)=>{
+app.post('/items', async (req,res)=>{
    console.log(req.body)
    const {name}=req.body
-  const newItem={id:Date.now(),name}
-   items.push(newItem)
+  
+  const newItem = await Item.create({item:name})
    res.json(newItem)
 })
-app.put('/items/:id',(req,res)=>{
-    const id = Number(req.params.id)
+app.put('/items/:id', async (req,res)=>{
+    const id = req.params.id
     const {name}=req.body
-    const existingItem=items.find(item=>item.id===id)
-    if(!existingItem){
+    const updatedItem=await Item.findByIdAndUpdate(id,{item:name},{new:true})
+    if(!updatedItem){
         return res.status(404).json({message :"Item not found "})
     }
-    items=items.map(item=>item.id===id?{...item,name}:item)
-    res.json({...existingItem,name})
+    res.json(updatedItem)
 })
-app.delete('/items/:id',(req,res)=>{
-    const id =Number(req.params.id)
-    const originallength=items.length
-    items=items.filter(item=>item.id!==id)
-    if(items.length===originallength){
-        return res.status(404).json({message:"item not found" })
-    }
+app.delete('/items/:id',async (req,res)=>{
+    const id =req.params.id
+   const deletedItem =await Item.findByIdAndDelete(id)
+   if(!deletedItem){
+    return res.status(404).json({message:"item not found"})
+   }
     res.json({message:"item deleted successfully "})
    
 })
 
-
+const PORT = process.env.PORT || 5001;
 app.listen(process.env.PORT,()=>{
      console.log("Server running on port", process.env.PORT)
 })
